da1da0083c689efe687f960dc498c66a1035f028
Flzzyf <flzzyf076@gmail.com>
2018-07-21 22:33:03 +0800

    自动增加金钱

--- TowerAttack/Assets/Script/GameManager.cs
+++ TowerAttack/Assets/Script/GameManager.cs
@@ -1,17 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
     public int player = 1;
+
+	public Text text_money;
+	int money = 0;
+
+	public float moneyIncreaseTime = 10;
+	float moneyIncreaseCD = 0;
 
 	void Start ()
     {
         MapManager.Instance().GenerateMap();
 	}
 	
-	void Update () {
-		
+	void Update () 
+	{
+		if(moneyIncreaseCD <= 0)
+		{
+			moneyIncreaseCD = moneyIncreaseTime;
+
+			IncreaseMoney();
+		}
+		else
+		{
+			moneyIncreaseCD -= Time.deltaTime;
+		}
+	}
+
+	void IncreaseMoney()
+	{
+		if(money == 0)
+		{
+			money++;
+		}
+		else
+		{
+			money *= 2;
+		}
+
+		text_money.text = money.ToString();
 	}
 }
