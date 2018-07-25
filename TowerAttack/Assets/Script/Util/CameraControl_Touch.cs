c2dadd7b273e57b3f6c6876311a74b76108b3870
Flzzyf <flzzyf076@gmail.com>
2018-07-25 14:12:28 +0800

    加入RTS镜头移动脚本

---
+++ TowerAttack/Assets/Script/Util/CameraControl_Touch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl_Touch : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+#if UNITY_STANDALONE
+        Debug.Log("PC版");
+
+        this.enabled = false;
+#endif
+    }
+
+
+    void Update () 
+	{
+		
+	}
+}
