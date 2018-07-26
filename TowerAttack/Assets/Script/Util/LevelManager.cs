3d86f42316ea3a6d684ca4116111ca406d77326a
Flzzyf <flzzyf076@gmail.com>
2018-07-26 16:32:15 +0800

    修改场景加载系统，加入字体

---
+++ TowerAttack/Assets/Script/Util/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour 
+{
+    public GameObject panel_loading;
+    public Slider slider;
+
+    public void LoadScene(string _name)
+    {
+        panel_loading.SetActive(true);
+
+        StartCoroutine(LoadSceneAsync(_name));
+    }
+
+    IEnumerator LoadSceneAsync(string _name)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_name);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+
+}
