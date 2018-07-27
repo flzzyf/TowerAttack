4402bccc84fde7869de5b168a64d185b240c7d1f
Flzzyf <flzzyf076@gmail.com>
2018-07-27 15:47:02 +0800

    各种游戏选项

---
+++ TowerAttack/Assets/Script/Managers/MenuManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour 
+{
+    public GameObject panel_loading;
+    public Slider slider;
+    public AudioMixer audioMixer;
+
+    float playSoundCD = 0.2f;
+    float currentPlaySoundCD;
+
+    public Dropdown dropdown_resolution;
+
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+        SoundManager.Instance().Play("BGM");
+
+        resolutions = Screen.resolutions;
+
+        //添加分辨率选项
+        dropdown_resolution.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+               resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        dropdown_resolution.AddOptions(options);
+        dropdown_resolution.value = currentResolutionIndex;
+    }
+
+    private void Update()
+    {
+        if (currentPlaySoundCD > 0)
+            currentPlaySoundCD -= Time.deltaTime;
+    }
+
+    public void LoadScene(string _name)
+    {
+        panel_loading.SetActive(true);
+
+        StartCoroutine(LoadSceneAsync(_name));
+    }
+
+    IEnumerator LoadSceneAsync(string _name)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_name);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    //设置BGM音量
+    public void SetVolumeBGM(float _value)
+    {
+        audioMixer.SetFloat("volume_bgm", _value);
+    }
+
+    public void SetVolumeEffect(float _value)
+    {
+        audioMixer.SetFloat("volume_effect", _value);
+
+        if(currentPlaySoundCD <= 0)
+        {
+            currentPlaySoundCD = playSoundCD;
+            SoundManager.Instance().Play("Boom");
+        }
+    }
+
+    public void ClickButton()
+    {
+        SoundManager.Instance().Play("Boom");
+    }
+
+    public void SetQuality(int _value)
+    {
+        QualitySettings.SetQualityLevel(_value);
+    }
+
+    public void SetFullScreen(bool _isFullScreen)
+    {
+        Screen.fullScreen = _isFullScreen;
+    }
+
+    public void SetResolution(int _value)
+    {
+        Resolution resolution = resolutions[_value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
---
+++ TowerAttack/Assets/Script/Util/SoundManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public Sound[] sounds = new Sound[1];
+
+
+    void Start () 
+	{
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            if (s.audioMixerGroup != null)
+                s.source.outputAudioMixerGroup = s.audioMixerGroup;
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void StopPlay(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Stop();
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1;
+    [Range(.1f, 3f)]
+    public float pitch = 1;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public AudioMixerGroup audioMixerGroup;
+}
+
